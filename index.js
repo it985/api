1b78eca261707c268b9678bed66e773306a4a385
zx <2071916845@qq.com>
2023-09-05 22:28:46 +0800

    git init

---
+++ index.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+    loadRandomImage();
+}
+
+function loadRandomImage() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState === 4 && xhttp.status === 200) {
+            var imageUrls = this.responseText.split("\n");
+            var randomIndex = Math.floor(Math.random() * imageUrls.length);
+            window.location.href = imageUrls[randomIndex];
+        } else {
+            console.log("图片文件状态异常，可联系foryouos@qq.com");
+        }
+    };
+    xhttp.open("GET", "demo.txt", true);
+    xhttp.send();
+}
